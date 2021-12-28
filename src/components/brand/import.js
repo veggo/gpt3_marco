8e6e5a2ea9b9cd41b5aac95fc58a3577f1f00783
veggo <veggo94@gmail.com>
2021-12-28 10:21:11 +0100

    Brand + images

---
+++ src/components/brand/import.js
@@ -0,0 +1,16 @@
+/*File per semplificare l'import delle immagini dei vari brand e poterle poi richiamare con una variabile 
+(prima però bisogn sempre fare l'import di questo file)*/
+
+import google from '../../assets/google.png';
+import slack from '../../assets/slack.png';
+import atlassian from '../../assets/atlassian.png';
+import dropbox from '../../assets/dropbox.png';
+import shopify from '../../assets/shopify.png';
+
+export {
+  google,
+  slack,
+  atlassian,
+  dropbox,
+  shopify,
+};
